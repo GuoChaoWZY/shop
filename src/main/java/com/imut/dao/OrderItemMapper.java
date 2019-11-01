a5c16c7c5a7fb3c5921e9d8a5f775f00d92a43fb
GuoChaoWZY <1352547676@qq.com>
2019-11-01 15:32:13 +0800

    project init commit

---
+++ src/main/java/com/imut/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.imut.dao;
+
+import com.imut.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
